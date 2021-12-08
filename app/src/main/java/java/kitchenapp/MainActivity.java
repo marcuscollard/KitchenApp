a411281476a1536739e393a4ec481c47f4c4c8d0
Marcus Collard <marcuscollard225@gmail.com>
2021-12-08 16:46:40 +0100

    test data not DB

--- app/src/main/java/java/kitchenapp/MainActivity.java
+++ app/src/main/java/java/kitchenapp/MainActivity.java
@@ -29,30 +29,29 @@ public class MainActivity extends AppCompatActivity {
 
 
         // FROM HARDCODED TEST ARRAY
-        //ArrayList<Order> test_orders = new ArrayList<Order>(5);
-        //test_orders.add(new Order(1, "fish", 20));
-        //test_orders.add(new Order(3, "potato", 5));
-        //test_orders.add(new Order(5, "salad", 5));
-        //test_orders.add(new Order(3, "McDonald's from next door", 10));
-        //test_orders.add(new Order(2, "chicken sushi", 10));
+        ArrayList<Order> test_orders = new ArrayList<Order>(5);
+        test_orders.add(new Order(1, "fish", 20));
+        test_orders.add(new Order(3, "potato", 5));
+        test_orders.add(new Order(5, "salad", 5));
+        test_orders.add(new Order(3, "McDonald's from next door", 10));
+        test_orders.add(new Order(2, "chicken sushi", 10));
 
         RecyclerView recycler = findViewById(R.id.recycler);
 
-        //SO.s.context = MainActivity.this;
+//        SO.s.context = MainActivity.this;
 
-        //orders = Order.populateOrders();
 
         SO.s.customAdapter = new OrdersCustomAdapter(MainActivity.this);
         recycler.setAdapter(SO.s.customAdapter);
         recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
-        //SO.s.addOrders(test_orders);
+        SO.s.addOrders(test_orders);
 
         // FROM DATABASE
-        XmlReaderTask xmlReaderTask = new XmlReaderTask();
-        xmlReaderTask.tableList = null;
-        xmlReaderTask.handler = new Handler();//Håller  koll på trådsom är ansvar för  nätverk
-        xmlReaderTask.execute();
+//        XmlReaderTask xmlReaderTask = new XmlReaderTask();
+//        xmlReaderTask.tableList = null;
+//        xmlReaderTask.handler = new Handler();//Håller  koll på trådsom är ansvar för  nätverk
+//        xmlReaderTask.execute();
 
         //SO.s.orders.add(new Order(2, "test", 0));
 
